eae6deefa770581012dfac78b78e1006ee7699cb
Michal Rembalski <31540274+mrembalski@users.noreply.github.com>
2024-06-08 11:40:08 +0200

    Add ESM

---
+++ structure_prediction/esm.py
@@ -0,0 +1,32 @@
+import sys
+import requests
+
+from Bio import SeqIO
+
+def run_esmfold(sequence):
+    url = "https://api.esmatlas.com/foldSequence/v1/pdb/"
+    response = requests.post(url, data=sequence, verify=False)
+    if response.status_code == 200:
+        pdb_data = response.text
+        return pdb_data
+    else:
+        print(f"Failed to fold sequence: {response.text}")
+
+def main(fasta_file):
+    for record in SeqIO.parse(fasta_file, "fasta"):
+        sequence = str(record.seq)
+        pdb_data = run_esmfold(sequence)
+        if pdb_data:
+            pdb_file_name = fasta_file.replace(".fasta", f".pdb")
+
+            with open(pdb_file_name, "w") as pdb_file:
+                pdb_file.write(pdb_data)
+            
+            print(f"Saved PDB output for {record.id} to {pdb_file_name}")
+
+        break
+
+if __name__ == "__main__":
+    assert len(sys.argv) == 2, "Usage: python esm.py <fasta_file>"
+    fasta_file = sys.argv[1]
+    main(fasta_file)
